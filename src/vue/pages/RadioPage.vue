9c4776d5a753256a9688b4495a14edaf5a249c1c
Bryan Hunt <bhunt@nvidia.com>
2025-05-30 20:38:48 -0500

    fix: add text for empty table

--- src/vue/pages/RadioPage.vue
+++ src/vue/pages/RadioPage.vue
@@ -10,18 +10,20 @@
         <TabPanels class="h-full">
           <TabPanel value="0" class="h-full p-4 overflow-y-auto">
             <div class="card">
-              <DataTable 
-                :value="program?.channels" 
-                size="small" 
+              <DataTable
+                :value="program?.channels"
+                size="small"
                 tableStyle="min-width: 50rem"
                 scrollable
-                scrollHeight="calc(100vh - 200px)"
-                class="h-full"
-              >
-                <Column field="channelNumber" header="Number"></Column>
-                <Column field="radioChannel.name" header="Name"></Column>
-                <Column field="radioChannel.transmitFrequency" header="Transmit"></Column>
-                <Column field="radioChannel.receiveFrequency" header="Receive"></Column>
+                scrollHeight="calc(100vh - 10rem)"
+                class="h-full">
+                <Column field="channelNumber" header="Number" />
+                <Column field="radioChannel.name" header="Name" />
+                <Column field="radioChannel.transmitFrequency" header="Transmit" />
+                <Column field="radioChannel.receiveFrequency" header="Receive" />
+                <template #empty>
+                  <div class="text-center text-gray-400">No radio data</div>
+                </template>
               </DataTable>
             </div>
           </TabPanel>
@@ -40,22 +42,34 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
-import { Column, DataTable, Tab, Tabs, TabList, TabPanels, TabPanel } from 'primevue';
-import { RadioMemory, RadioProgram, RadioId } from '@springfield/ham-radio-api';
-import HexDump from '../components/HexDump.vue';
-import { useRadioStore } from '../stores/radios';
+import { onMounted, ref } from "vue";
+import {
+  Column,
+  DataTable,
+  Tab,
+  Tabs,
+  TabList,
+  TabPanels,
+  TabPanel,
+} from "primevue";
+import { RadioMemory, RadioProgram, RadioId } from "@springfield/ham-radio-api";
+import HexDump from "../components/HexDump.vue";
+import { useRadioStore } from "../stores/radios";
 const radioId = ref<RadioId>();
 const memory = ref<RadioMemory>();
 const program = ref<RadioProgram>();
 const radioStore = useRadioStore();
 
 onMounted(() => {
-  window.radio.onRadioMemory((_event, radioId: RadioId, radioMemory: RadioMemory) => {
-    console.log('radioMemory', radioId);
-    radioId.value = radioId;
-    memory.value = radioMemory;
-    program.value = radioStore.radiosById.get(radioId.model)?.decodeMemory(radioMemory);
-  });
+  window.radio.onRadioMemory(
+    (_event, radioId: RadioId, radioMemory: RadioMemory) => {
+      console.log("radioMemory", radioId);
+      radioId.value = radioId;
+      memory.value = radioMemory;
+      program.value = radioStore.radiosById
+        .get(radioId.model)
+        ?.decodeMemory(radioMemory);
+    }
+  );
 });
 </script>
